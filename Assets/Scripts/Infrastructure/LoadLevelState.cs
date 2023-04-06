ef514eb472b85a8e05846408a17adccd74e578cd
Roman Ridel <ridel_rv@mail.ru>
2023-04-06 17:43:22 +0300

    Add Loading Screen

--- Assets/Scripts/Infrastructure/LoadLevelState.cs
+++ Assets/Scripts/Infrastructure/LoadLevelState.cs
@@ -1,3 +1,4 @@
+using Roguelike.Logic;
 using UnityEngine;
 
 namespace Roguelike.Infrastructure
@@ -8,26 +9,32 @@ namespace Roguelike.Infrastructure
         private const string PlayerPath = "Player/Player";
         private readonly GameStateMachine _stateMachine;
         private readonly SceneLoader _sceneLoader;
+        private readonly LoadingScreen _loadingScreen;
 
-        public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader)
+        public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader, LoadingScreen loadingScreen)
         {
             _stateMachine = stateMachine;
             _sceneLoader = sceneLoader;
+            _loadingScreen = loadingScreen;
         }
 
         public void Enter(string sceneName)
         {
+            _loadingScreen.Show();
             _sceneLoader.Load(sceneName, OnLoaded);
         }
 
         public void Exit()
         {
+            _loadingScreen.Hide();
         }
 
         private void OnLoaded()
         {
             GameObject initialPoint = GameObject.FindWithTag(InitialPointTag);
             GameObject player = Instantiate(PlayerPath, initialPoint.transform.position);
+            
+            _stateMachine.Enter<GameLoopState>();
         }
 
         private static GameObject Instantiate(string path)
