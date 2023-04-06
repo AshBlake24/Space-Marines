4892a3b92474f15fcc3d9547974cbafa5ed6b497
Roman Ridel <ridel_rv@mail.ru>
2023-04-06 17:16:08 +0300

    Fix Scene Loader

--- Assets/Scripts/Infrastructure/LoadLevelState.cs
+++ Assets/Scripts/Infrastructure/LoadLevelState.cs
@@ -1,7 +1,11 @@
+using UnityEngine;
+
 namespace Roguelike.Infrastructure
 {
     public class LoadLevelState : IPayloadedState<string>
     {
+        private const string InitialPointTag = "InitialPoint";
+        private const string PlayerPath = "Player/Player";
         private readonly GameStateMachine _stateMachine;
         private readonly SceneLoader _sceneLoader;
 
@@ -11,11 +15,31 @@ namespace Roguelike.Infrastructure
             _sceneLoader = sceneLoader;
         }
 
-        public void Enter(string sceneName) => 
-            _sceneLoader.Load(sceneName);
+        public void Enter(string sceneName)
+        {
+            _sceneLoader.Load(sceneName, OnLoaded);
+        }
 
         public void Exit()
         {
+        }
+
+        private void OnLoaded()
+        {
+            GameObject initialPoint = GameObject.FindWithTag(InitialPointTag);
+            GameObject player = Instantiate(PlayerPath, initialPoint.transform.position);
+        }
+
+        private static GameObject Instantiate(string path)
+        {
+            GameObject prefab = Resources.Load<GameObject>(path);
+            return Object.Instantiate(prefab);
+        }
+        
+        private static GameObject Instantiate(string path, Vector3 postition)
+        {
+            GameObject prefab = Resources.Load<GameObject>(path);
+            return Object.Instantiate(prefab, postition, Quaternion.identity);
         }
     }
 }
