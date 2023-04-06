0f4d58fadc358f60b1370e6daf01dde92ebc3d53
Roman Ridel <ridel_rv@mail.ru>
2023-04-06 13:16:32 +0300

    Add Scene Loader

--- Assets/Scripts/Infrastructure/GameBootstrapper.cs
+++ Assets/Scripts/Infrastructure/GameBootstrapper.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace Roguelike.Infrastructure
 {
-    public class GameBootstrapper : MonoBehaviour
+    public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
     {
         private Game _game;
 
