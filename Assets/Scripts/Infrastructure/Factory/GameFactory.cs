c4b575eab69816866f5a6bd0f982ad3b7419e327
Roman Ridel <ridel_rv@mail.ru>
2023-04-11 13:31:56 +0300

    Add Game Factory

---
+++ Assets/Scripts/Infrastructure/Factory/GameFactory.cs
@@ -0,0 +1,18 @@
+using Roguelike.Infrastructure.AssetManagement;
+using UnityEngine;
+
+namespace Roguelike.Infrastructure.Factory
+{
+    public class GameFactory : IGameFactory
+    {
+        private readonly IAssetProvider _assetProvider;
+
+        public GameFactory(IAssetProvider assetProvider)
+        {
+            _assetProvider = assetProvider;
+        }
+        
+        public GameObject CreatePlayer(Transform playerInitialPoint) => 
+            _assetProvider.Instantiate(AssetPath.PlayerPath, playerInitialPoint.position);
+    }
+}
---
+++ Assets/Scripts/Infrastructure/Factory/IGameFactory.cs
@@ -0,0 +1,10 @@
+using Roguelike.Infrastructure.Services;
+using UnityEngine;
+
+namespace Roguelike.Infrastructure.Factory
+{
+    public interface IGameFactory : IService
+    {
+        GameObject CreatePlayer(Transform playerInitialPoint);
+    }
+}
