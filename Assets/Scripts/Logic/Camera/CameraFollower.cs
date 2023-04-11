66beba843432990f41a33001e4775e779c2b7baf
Roman Ridel <ridel_rv@mail.ru>
2023-04-11 17:17:20 +0300

    Update player movement

--- Assets/Scripts/Logic/Camera/CameraFollower.cs
+++ Assets/Scripts/Logic/Camera/CameraFollower.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Roguelike.Logic.Camera
@@ -5,31 +6,24 @@ namespace Roguelike.Logic.Camera
     public class CameraFollower : MonoBehaviour
     {
         [SerializeField] private Transform _following;
-        [SerializeField] private float _rotationAngleX;
-        [SerializeField] private float _offsetY;
-        [SerializeField] private int _distance;
+        [SerializeField] private Vector3 _positionOffset;
+        [SerializeField] private Vector3 _rotationOffset;
 
         private void LateUpdate()
         {
             if(_following == null)
                 return;
             
-            Quaternion rotation = Quaternion.Euler(_rotationAngleX, 0, 0);
-            Vector3 position = rotation * new Vector3(0, 0, -_distance) + FollowingPointPosition();
-            transform.rotation = rotation;
-            transform.position = position;
+            transform.position = _following.position + _positionOffset;;
+            transform.rotation = Quaternion.Euler(_rotationOffset);
         }
 
         public void Follow(GameObject following)
         {
+            if (following == null)
+                throw new ArgumentNullException(nameof(following), "Following object can't be null!");
+            
             _following = following.transform;
-        }
-        
-        private Vector3 FollowingPointPosition()
-        {
-            Vector3 followingPosition = _following.position;
-            followingPosition.y += _offsetY;
-            return followingPosition;
         }
     }
 }
