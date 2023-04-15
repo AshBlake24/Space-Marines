c387b1535ee1828c00f76552ebe9e4ab1d7b33d9
Roman Ridel <ridel_rv@mail.ru>
2023-04-15 22:35:05 +0300

    Update weapons

--- Assets/Scripts/Weapons/IWeapon.cs
+++ Assets/Scripts/Weapons/IWeapon.cs
@@ -1,9 +1,15 @@
+using UnityEngine;
 using Roguelike.Weapons.Stats;
 
 namespace Roguelike.Weapons
 {
     public interface IWeapon
     {
+        WeaponStats Stats { get; }
+        Vector3 PositionOffset { get; }
+        Vector3 RotationOffset { get; }
         void Attack();
+        void Show();
+        void Hide();
     }
 }
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -5,6 +5,15 @@ namespace Roguelike.Weapons
 {
     public abstract class Weapon : MonoBehaviour, IWeapon
     {
+        [SerializeField] private Vector3 _positionOffset;
+        [SerializeField] private Vector3 _rotationOffset;
+
+        public Vector3 PositionOffset => _positionOffset;
+        public Vector3 RotationOffset => _rotationOffset;
+        
+        public abstract WeaponStats Stats { get; }
         public abstract void Attack();
+        public void Show() => gameObject.SetActive(true);
+        public void Hide() => gameObject.SetActive(false);
     }
 }
