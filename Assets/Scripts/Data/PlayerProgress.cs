067f8ede986c1267b35a2eb8f4ceab85b88a993c
Roman Ridel <ridel_rv@mail.ru>
2023-04-15 17:22:24 +0300

    Update Persistent Data

--- Assets/Scripts/Data/PlayerProgress.cs
+++ Assets/Scripts/Data/PlayerProgress.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Roguelike.Weapons;
 
 namespace Roguelike.Data
 {
@@ -6,10 +8,12 @@ namespace Roguelike.Data
     public class PlayerProgress
     {
         public WorldData WorldData;
+        public WeaponsData WeaponsData;
 
-        public PlayerProgress(string initialLevel)
+        public PlayerProgress(string initialLevel, IWeapon startWeapon)
         {
             WorldData = new WorldData(initialLevel);
+            WeaponsData = new WeaponsData(startWeapon);
         }
     }
 }
