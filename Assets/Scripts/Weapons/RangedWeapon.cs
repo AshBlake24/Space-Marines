f468bebda926844ba61191b58acff2cd1ef61a31
Roman Ridel <ridel_rv@mail.ru>
2023-04-15 22:35:05 +0300

    Update weapons

--- Assets/Scripts/Weapons/RangedWeapon.cs
+++ Assets/Scripts/Weapons/RangedWeapon.cs
@@ -1,18 +1,21 @@
+using Roguelike.Data;
+using Roguelike.Infrastructure.Services.PersistentData;
 using Roguelike.Weapons.Stats;
 using UnityEngine;
 
 namespace Roguelike.Weapons
 {
-    public class RangedWeapon : Weapon
+    public class RangedWeapon : Weapon, IProgressWriter
     {
         [SerializeField] private Transform _firePoint;
 
         private RangedWeaponStats _stats;
-        
+
+        public override WeaponStats Stats => _stats;
         public int CurrentAmmo { get; private set; }
         public int CurrentClipAmmo { get; private set; }
         public float LastShotTime { get; private set; }
-        
+
         public void Construct(RangedWeaponStats stats)
         {
             _stats = stats;
@@ -23,6 +26,20 @@ namespace Roguelike.Weapons
         public override void Attack()
         {
             throw new System.NotImplementedException();
+        }
+
+        public void WriteProgress(PlayerProgress progress) => 
+            progress.PlayerWeapons.SaveRangedWeapon(Stats.ID, CurrentAmmo, CurrentClipAmmo);
+
+        public void ReadProgress(PlayerProgress progress)
+        {
+            RangedWeaponsData weaponsData = progress.PlayerWeapons.RangedWeapons.Find(x => x.ID == Stats.ID);
+            
+            if (weaponsData == null)
+                return;
+
+            CurrentAmmo = weaponsData.CurrentAmmo;
+            CurrentClipAmmo = weaponsData.CurrentClipAmmo;
         }
     }
 }
