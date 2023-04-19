b5c496d977c47941baceb33661b703a7a4e2822b
Roman Ridel <ridel_rv@mail.ru>
2023-04-20 01:31:48 +0300

    Update helpers pool functions

--- Assets/Scripts/Utilities/Helpers.cs
+++ Assets/Scripts/Utilities/Helpers.cs
@@ -7,6 +7,7 @@ namespace Roguelike.Utilities
     public static class Helpers
     {
         #region UI
+
         private static PointerEventData s_eventDataCurrentPosition;
         private static List<RaycastResult> s_results;
 
@@ -18,9 +19,11 @@ namespace Roguelike.Utilities
 
             return s_results.Count > 0;
         }
+
         #endregion
 
         #region Time
+
         private static readonly Dictionary<float, WaitForSeconds> s_waitDictionary =
             new Dictionary<float, WaitForSeconds>();
 
@@ -33,18 +36,37 @@ namespace Roguelike.Utilities
 
             return s_waitDictionary[timeInSeconds];
         }
+
         #endregion Time
-        
+
         #region Pool
-        private static Transform s_generalPoolsContainer;
 
-        public static Transform GetGeneralPoolsContainer()
+        private static readonly Transform s_generalPoolsContainer = new GameObject($"Pools").transform;
+        private static readonly Dictionary<string, Transform> s_pools = new();
+
+        public static Transform GetGeneralPoolsContainer() => 
+            s_generalPoolsContainer;
+
+        public static Transform GetPoolsContainer(string name)
         {
-            if (s_generalPoolsContainer == null)
-                s_generalPoolsContainer = new GameObject($"Pools").transform;
+            if (s_pools.ContainsKey(name) == false)
+                SetPoolsContainer(name);
 
-            return s_generalPoolsContainer;
+            return s_pools[name];
         }
+
+        private static void SetPoolsContainer(string name)
+        {
+            if (s_pools.ContainsKey(name) == false)
+            {
+                s_pools.Add(name, new GameObject($"Pool - {name}").transform);
+                SetGeneralsPoolContainer(s_pools[name]);
+            }
+        }
+
+        private static void SetGeneralsPoolContainer(Transform pool) =>
+            pool.SetParent(s_generalPoolsContainer);
+
         #endregion
     }
 }
