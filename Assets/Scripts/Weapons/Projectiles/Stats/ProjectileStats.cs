453f49e8cc8b97a3e6ac8b2a445b92131fcb7121
Roman Ridel <ridel_rv@mail.ru>
2023-04-19 12:15:32 +0300

    Delete unuseful projectile types & moved vfx data to static data

--- Assets/Scripts/Weapons/Projectiles/Stats/ProjectileStats.cs
+++ Assets/Scripts/Weapons/Projectiles/Stats/ProjectileStats.cs
@@ -1,4 +1,5 @@
 using Roguelike.StaticData.Projectiles;
+using UnityEngine;
 
 namespace Roguelike.Weapons.Projectiles.Stats
 {
@@ -7,16 +8,25 @@ namespace Roguelike.Weapons.Projectiles.Stats
         private readonly int _damage;
         private readonly float _speed;
         private readonly float _lifetime;
+        private readonly ParticleSystem _muzzleFlashVFX;
+        private readonly ParticleSystem _projectileVFX;
+        private readonly ParticleSystem _impactVFX;
 
         protected ProjectileStats(ProjectileStaticData staticData)
         {
             _damage = staticData.Damage;
             _speed = staticData.Speed;
             _lifetime = staticData.Lifetime;
+            _muzzleFlashVFX = staticData.MuzzleFlashVFX;
+            _projectileVFX = staticData.ProjectileVFX;
+            _impactVFX = staticData.ImpactVFX;
         }
         
         public int Damage => _damage;
         public float Speed => _speed;
         public float Lifetime => _lifetime;
+        public ParticleSystem MuzzleFlashVFX => _muzzleFlashVFX;
+        public ParticleSystem ProjectileVFX => _projectileVFX;
+        public ParticleSystem ImpactVFX => _impactVFX;
     }
 }
