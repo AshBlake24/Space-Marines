3ccb5b8cd255bdf37504320722c83a9a4a83690d
Roman Ridel <ridel_rv@mail.ru>
2023-04-20 17:47:58 +0300

    Add health bar

---
+++ Assets/Scripts/UI/ActorUI.cs
@@ -0,0 +1,27 @@
+using System;
+using Roguelike.Player;
+using UnityEngine;
+
+namespace Roguelike.UI
+{
+    public class ActorUI : MonoBehaviour
+    {
+        [SerializeField] private HealthBar _healthBar;
+
+        private PlayerHealth _playerHealth;
+
+        private void OnDestroy() => 
+            _playerHealth.HealthChanged -= OnHealthChanged;
+
+        public void Construct(PlayerHealth playerHealth)
+        {
+            _playerHealth = playerHealth;
+            _playerHealth.HealthChanged += OnHealthChanged;
+        }
+        
+        private void OnHealthChanged()
+        {
+            _healthBar.SetValue(_playerHealth.CurrentHealth, _playerHealth.MaxHealth);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Roguelike.UI
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Image _currentHealthImage;
+        [SerializeField] private TextMeshProUGUI _currentHealthText;
+
+        public void SetValue(int current, int max)
+        {
+            _currentHealthImage.fillAmount = (float) current / max;
+            _currentHealthText.text = $"{current}/{max}";
+        }
+    }
+}
