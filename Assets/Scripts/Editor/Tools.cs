704c4f6ec17dc41f21b93d6098067ee77c7da7ec
Roman Ridel <ridel_rv@mail.ru>
2023-04-21 14:03:28 +0300

    Move saving to json file from SaveLoadService to Editor Tools

--- Assets/Scripts/Editor/Tools.cs
+++ Assets/Scripts/Editor/Tools.cs
@@ -1,3 +1,8 @@
+using System.IO;
+using Roguelike.Data;
+using Roguelike.Infrastructure.Services;
+using Roguelike.Infrastructure.Services.PersistentData;
+using Roguelike.Infrastructure.Services.SaveLoad;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,6 +15,39 @@ namespace Roguelike.Editor
         {
             PlayerPrefs.DeleteAll();
             PlayerPrefs.Save();
+        }
+
+        
+        [MenuItem("Tools/Save Progress To JSON")]
+        public static void SaveProgress()
+        {
+            if (EditorApplication.isPlaying == false)
+                return;
+            
+            AllServices.Container
+                .Single<ISaveLoadService>()
+                .SaveProgress();
+
+            string dataToStore = AllServices.Container
+                .Single<IPersistentDataService>()
+                .ToJson();
+            
+            SaveToFile(dataToStore);
+            
+            Debug.Log("Progress Saved");
+        }
+        
+        private static void SaveToFile(string dataToStore)
+        {
+            string path = Path.Combine(Application.persistentDataPath, "Data.json");
+
+            using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fileStream))
+                {
+                    streamWriter.Write(dataToStore);
+                }
+            }
         }
     }
 }
