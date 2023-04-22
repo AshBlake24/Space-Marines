e059ff0347a7d87c135f8398075f3ba48fbb1b6e
Roman Ridel <ridel_rv@mail.ru>
2023-04-22 14:27:43 +0300

    Refactor projectiles

--- Assets/Scripts/Weapons/Projectiles/ExplodingProjectile.cs
+++ Assets/Scripts/Weapons/Projectiles/ExplodingProjectile.cs
@@ -8,49 +8,21 @@ namespace Roguelike.Weapons.Projectiles
     public class ExplodingProjectile : Projectile
     {
         private ExplodingProjectileStats _stats;
-        private string _impactVFXKey;
-        
+
+        protected override ProjectileStats Stats => _stats;
+
         public override void Construct<TStats>(TStats stats, IObjectPool<Projectile> projectilePool)
         {
-            base.Construct(stats, projectilePool);
             InitializeExplodingProjectileStats(stats);
-            CreateImpactVFXPool();
-            CreateProjectileVFX();
+            base.Construct(stats, projectilePool);
         }
 
         private void OnCollisionEnter(Collision collision)
         {
-            SpawnVFX(_impactVFXKey);
+            SpawnVFX(ImpactVFXKey);
             ReturnToPool();
         }
-
-        public override void Init()
-        {
-            Rigidbody.velocity = transform.forward * _stats.Speed;
-            AccumulatedTime = 0f;
-            ProjectileVFX.Play();
-        }
-
-        protected override void LifetimeTick()
-        {
-            AccumulatedTime += Time.deltaTime;
-
-            if (AccumulatedTime >= _stats.Lifetime)
-                ReturnToPool();
-        }
         
-        protected override void CreateImpactVFXPool()
-        {
-            _impactVFXKey = _stats.ProjectileVFX.gameObject.name;
-            ParticlesPool.CreateNewPool(_impactVFXKey, _stats.ImpactVFX);
-        }
-
-        protected override void CreateProjectileVFX()
-        {
-            ProjectileVFX = Instantiate(_stats.ProjectileVFX, transform.position, transform.rotation, transform);
-            StopProjectileVFX();
-        }
-
         private void InitializeExplodingProjectileStats<TStats>(TStats stats)
         {
             if (stats is ExplodingProjectileStats explodingStats)
