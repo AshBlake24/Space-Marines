a614076660f45f2831c3a87934808c43a2310f0a
Roman Ridel <ridel_rv@mail.ru>
2023-04-24 12:01:47 +0300

    Start working on player skills

---
+++ Assets/Scripts/Player/PlayerSkill.cs
@@ -0,0 +1,25 @@
+using Roguelike.Infrastructure.Services;
+using Roguelike.Infrastructure.Services.Input;
+using Roguelike.Player.Skills;
+using UnityEngine;
+
+namespace Roguelike.Player
+{
+    public class PlayerSkill : MonoBehaviour
+    {
+        private IInputService _input;
+        private ISkill _skill;
+
+        private void Awake() =>
+            _input = AllServices.Container.Single<IInputService>();
+
+        public void Construct(ISkill skill) =>
+            _skill = skill;
+
+        private void OnUseSkill()
+        {
+            if (_skill.ReadyToUse && _skill.IsActive == false)
+                _skill.UseSkill();
+        }
+    }
+}
