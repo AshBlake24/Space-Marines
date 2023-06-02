e3eb8d1f9f40b0d8a2fd7dd05806fea6d9e581f1
kovalenkodaniil <kovalenkodaniilv@gmail.com>
2023-06-02 14:59:02 +0300

    manual restore Kamikadze anim

--- Assets/Scripts/Enemies/EnemyStates/ExplosionState.cs
+++ Assets/Scripts/Enemies/EnemyStates/ExplosionState.cs
@@ -1,15 +1,50 @@
 ﻿
 using Roguelike.Roguelike.Enemies.Animators;
+using System.Collections;
+using UnityEngine;
 
 namespace Roguelike.Enemies.EnemyStates
 {
     public class ExplosionState : EnemyState
     {
+        private static readonly float ClipDuration = 2.1f;
+
+        [SerializeField, Range(0.5f, 3f)] private float _delayForAnimation;
+
+        private Coroutine _delayCoroutine;
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            if (_delayCoroutine != null)
+                StopCoroutine(_delayCoroutine);
+        }
+
         public override void Enter(Enemy enemy, EnemyAnimator enemyAnimator)
         {
             base.Enter(enemy, enemyAnimator);
 
             enemy.Target.TakeDamage(enemy.Damage);
+
+            animator.PlayAttack();
+
+            _delayCoroutine = StartCoroutine(Explosion());
+
+            EnemyHealth health = GetComponentInChildren<EnemyHealth>();
+            health.TakeDamage(health.MaxHealth);
+        }
+
+        private IEnumerator Explosion()
+        {
+            float duration = _delayForAnimation;
+
+            while (duration > 0)
+            {
+                duration -= Time.deltaTime;
+
+                yield return null;
+            }
 
             EnemyHealth health = GetComponentInChildren<EnemyHealth>();
             health.TakeDamage(health.MaxHealth);
