4af1086d1be518cb0ea113be5e943b3d689e45bc
kovalenkodaniil <kovalenkodaniilv@gmail.com>
2023-06-05 18:01:46 +0300

    Update enemy prefab

--- Assets/Scripts/Enemies/EnemyStates/ExplosionState.cs
+++ Assets/Scripts/Enemies/EnemyStates/ExplosionState.cs
@@ -1,52 +1,28 @@
 ﻿
 using Roguelike.Roguelike.Enemies.Animators;
 using System.Collections;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 namespace Roguelike.Enemies.EnemyStates
 {
     public class ExplosionState : EnemyState
     {
-        private static readonly float ClipDuration = 2.1f;
-
-        [SerializeField, Range(0.5f, 3f)] private float _delayForAnimation;
         [SerializeField] private ParticleSystem _effects;
-
-        private Coroutine _delayCoroutine;
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-
-            if (_delayCoroutine != null)
-                StopCoroutine(_delayCoroutine);
-        }
 
         public override void Enter(Enemy enemy, EnemyAnimator enemyAnimator)
         {
             base.Enter(enemy, enemyAnimator);
 
-            enemy.Target.TakeDamage(enemy.Damage);
-
-            animator.SetClipSpeed(ClipDuration / _delayForAnimation);
             animator.PlayAttack();
-
-            _delayCoroutine = StartCoroutine(Explosion());
         }
 
-        private IEnumerator Explosion()
+        public void Explosion()
         {
-            float duration = _delayForAnimation;
-
-            while (duration > 0)
-            {
-                duration -= Time.deltaTime;
-
-                yield return null;
-            }
-
             EnemyHealth health = GetComponentInChildren<EnemyHealth>();
             health.TakeDamage(health.MaxHealth);
+
+            enemy.Target.TakeDamage(enemy.Damage);
 
             if (_effects != null)
                 _effects.Play();
