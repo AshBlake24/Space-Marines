65a254788c29cea85fa5a6ca75258c42a8000028
Roman Ridel <ridel_rv@mail.ru>
2023-06-14 20:31:15 +0300

    Add spear traps

---
+++ Assets/Scripts/Enemies/Traps/Spear.cs
@@ -0,0 +1,25 @@
+﻿using Roguelike.Player;
+using UnityEngine;
+
+namespace Roguelike.Enemies.Traps
+{
+    [RequireComponent(typeof(Collider))]
+    public class Spear : MonoBehaviour
+    {
+        [SerializeField] private Collider _collider;
+        
+        private int _damage;
+
+        public void Init(int damage) => 
+            _damage = damage;
+
+        public void SwitchState(bool isActive) => 
+            _collider.enabled = isActive;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.TryGetComponent(out PlayerHealth playerHealth)) 
+                playerHealth.TakeDamage(_damage);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/Traps/SpearTrap.cs
@@ -0,0 +1,73 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Roguelike.Enemies.Traps
+{
+    public class SpearTrap : MonoBehaviour
+    {
+        [SerializeField] private Spear[] _spears;
+        [SerializeField] private int _damage = 1;
+        [SerializeField] private float _depth = 1.75f;
+        [SerializeField] private float _switchTimer = 2f;
+        [SerializeField] private float _switchingAnimationDuration = 0.25f;
+
+        private Vector3 _onPosition;
+        private Vector3 _offPosition;
+        private bool _isActive;
+        private float _timer;
+
+        private void Start() => 
+            InitSpears();
+
+        private void OnValidate()
+        {
+            if (_switchingAnimationDuration > _switchTimer)
+                _switchingAnimationDuration = _switchTimer;
+        }
+
+        private void InitSpears()
+        {
+            _onPosition = transform.position;
+            _offPosition = transform.position;
+            _offPosition.y -= _depth;
+            transform.position = _offPosition;
+            _isActive = false;
+            
+            foreach (Spear spear in _spears)
+                spear.Init(_damage);
+            
+            SwitchColliders();
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer >= _switchTimer)
+            {
+                SwitchState();
+                _timer = 0f;
+            }
+        }
+
+        private void SwitchState()
+        {
+            _isActive = !_isActive;
+
+            Vector3 targetPosition = _isActive 
+                ? _onPosition 
+                : _offPosition;
+
+            transform.DOMove(targetPosition, _switchingAnimationDuration)
+                .SetEase(Ease.OutBounce);
+
+            SwitchColliders();
+        }
+
+        private void SwitchColliders()
+        {
+            foreach (Spear spear in _spears)
+                spear.SwitchState(_isActive);
+        }
+    }
+}
