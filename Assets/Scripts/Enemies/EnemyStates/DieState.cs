82b48250aba6f1db08f8493911f6977407612db6
kovalenkodaniil <kovalenkodaniilv@gmail.com>
2023-06-16 23:25:47 +0300

    refactoring enemy state and enemy animation controller

--- Assets/Scripts/Enemies/EnemyStates/DieState.cs
+++ Assets/Scripts/Enemies/EnemyStates/DieState.cs
@@ -1,46 +1,24 @@
 ﻿using Roguelike.Roguelike.Enemies.Animators;
-using System.Collections;
-using UnityEngine;
+using Roguelike.StaticData.Enemies;
 
 namespace Roguelike.Enemies.EnemyStates
 {
     public class DieState : EnemyState
     {
-        private static readonly float ClipDuration = 2f;
-
-        [SerializeField, Range(0.5f, 3f)] private float _delayForAnimation;
-
-        private Coroutine _delayCoroutine;
-
-        protected override void OnDisable()
-        {
-            base.OnDisable();
-
-            if (_delayCoroutine != null)
-                StopCoroutine(_delayCoroutine);
-        }
+        private const EnemyId Mine = EnemyId.Mine;
 
         public override void Enter(Enemy curentEnemy, EnemyAnimator enemyAnimator)
         {
             base.Enter(curentEnemy, enemyAnimator);
 
-            _delayCoroutine = StartCoroutine(Die());
-
-            animator.SetClipSpeed(ClipDuration / _delayForAnimation);
-            animator.PlayDie();
+            if (enemy.Id == Mine)
+                Die();
+            else
+                animator.PlayDie();
         }
 
-        private IEnumerator Die()
+        public void Die()
         {
-            float duration = _delayForAnimation;
-
-            while (duration > 0)
-            {
-                duration -= Time.deltaTime;
-
-                yield return null;
-            }
-
             Destroy(gameObject);
         }
     }
