681e3aed473149373d027154a86b1d055e907908
Roman Ridel <ridel_rv@mail.ru>
2023-06-20 13:38:05 +0300

    Refactor CSVLoader.cs

--- Assets/Scripts/Localization/CSVLoader.cs
+++ Assets/Scripts/Localization/CSVLoader.cs
@@ -8,9 +8,10 @@ namespace Roguelike.Localization
     public class CSVLoader
     {
         private readonly char _lineSeparator = '\n';
-        private readonly char _surround = '"';
+        private readonly char _textSurroundings = '"';
         private readonly string[] _fieldSeparator = { "\",\""};
-        
+        private readonly Regex _csvParser = new(",(?(:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+
         private TextAsset _csvFile;
 
         public void LoadCSV(string csvPath) => 
@@ -18,13 +19,14 @@ namespace Roguelike.Localization
 
         public Dictionary<string, string> GetDictionaryValues(string attributeId)
         {
-            Dictionary<string, string> dictionary = new();
-
             string[] lines = _csvFile.text.Split(_lineSeparator);
             string[] headers = lines[0].Split(_fieldSeparator, StringSplitOptions.None);
             int attributeIndex = GetAttributeIndex(attributeId, headers);
 
-            ParseTextToDictionary(dictionary, lines, attributeIndex);
+            if (attributeIndex == -1)
+                throw new ArgumentOutOfRangeException(nameof(attributeId), $"Attribute \"{attributeId}\" not found");
+
+            Dictionary<string, string> dictionary = ParseTextToDictionary(lines, attributeIndex);
 
             return dictionary;
         }
@@ -45,26 +47,6 @@ namespace Roguelike.Localization
             return attributeIndex;
         }
 
-        private void ParseTextToDictionary(Dictionary<string, string> dictionary, string[] lines, int attributeIndex)
-        {
-            Regex csvParser = new(",(?(:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-
-            for (int i = 1; i < lines.Length; i++)
-            {
-                string line = lines[i];
-                string[] fields = csvParser.Split(line);
-
-                for (int j = 0; j < fields.Length; j++)
-                {
-                    fields[j] = fields[j].TrimStart(' ', _surround);
-                    fields[j] = fields[j].TrimEnd(_surround);
-                }
-
-                if (fields.Length > attributeIndex) 
-                    TryAddValue(dictionary, fields, attributeIndex);
-            }
-        }
-
         private static void TryAddValue(Dictionary<string, string> dictionary, string[] fields, int attributeIndex)
         {
             string key = fields[0];
@@ -75,6 +57,28 @@ namespace Roguelike.Localization
             string value = fields[attributeIndex];
 
             dictionary.Add(key, value);
+        }
+
+        private Dictionary<string, string> ParseTextToDictionary(string[] lines, int attributeIndex)
+        {
+            Dictionary<string, string> dictionary = new();
+            
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] fields = _csvParser.Split(line);
+
+                for (int j = 0; j < fields.Length; j++)
+                {
+                    fields[j] = fields[j].TrimStart(' ', _textSurroundings);
+                    fields[j] = fields[j].TrimEnd(_textSurroundings);
+                }
+
+                if (fields.Length > attributeIndex) 
+                    TryAddValue(dictionary, fields, attributeIndex);
+            }
+
+            return dictionary;
         }
     }
 }
