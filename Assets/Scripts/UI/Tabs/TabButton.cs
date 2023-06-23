8f40bc15ba081b3f1b98f774d5562341352fa619
Roman Ridel <ridel_rv@mail.ru>
2023-06-23 23:44:00 +0300

    Update UI elements

--- Assets/Scripts/UI/Tabs/TabButton.cs
+++ Assets/Scripts/UI/Tabs/TabButton.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using Roguelike.Localization;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,14 +9,13 @@ namespace Roguelike.UI.Tabs
     public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
     {
         [SerializeField] private TabGroup _tabGroup;
-        private string _label;
+        [SerializeField] private LocalizedString _localizedString;
         
         public TextMeshProUGUI Text { get; private set; }
 
         private void Awake()
         {
             Text = GetComponent<TextMeshProUGUI>();
-            _label = Text.text;
             _tabGroup.Subscribe(this);
         }
 
@@ -28,8 +28,8 @@ namespace Roguelike.UI.Tabs
         public void OnPointerClick(PointerEventData eventData) => 
             _tabGroup.OnTabSelected(this);
 
-        public void OnSelected() => Text.text = $"<u>{_label}</u>";
+        public void OnSelected() => Text.text = $"<u>{_localizedString.Value}</u>";
 
-        public void OnDeselected() => Text.text = _label;
+        public void OnDeselected() => Text.text = _localizedString.Value;
     }
 }
