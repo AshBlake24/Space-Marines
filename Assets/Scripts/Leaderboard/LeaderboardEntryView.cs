cbc7f29ebf4b7faeeaef1b0da198781d5db4468f
Roman Ridel <ridel_rv@mail.ru>
2023-06-23 12:41:28 +0300

    Add leaderboard views

---
+++ Assets/Scripts/Leaderboard/LeaderboardEntryView.cs
@@ -0,0 +1,20 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Roguelike.Leaderboard
+{
+    public class LeaderboardEntryView : MonoBehaviour
+    {
+        [Header("View")]
+        [SerializeField] private Image _background;
+        [SerializeField] private Color _evenColor;
+        [SerializeField] private Color _oddColor;
+        
+        [Header("Player Info")]
+        [SerializeField] private Image _avatar;
+        [SerializeField] private TextMeshProUGUI _name;
+        [SerializeField] private TextMeshProUGUI _rank;
+        [SerializeField] private TextMeshProUGUI _score;
+    }
+}
