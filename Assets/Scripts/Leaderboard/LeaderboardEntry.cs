9330e2842c3b5fcc5d245ef8cb70dd86715259b5
Roman Ridel <ridel_rv@mail.ru>
2023-06-27 18:59:22 +0300

    Add fetching profile avatar form SDK

--- Assets/Scripts/Leaderboard/LeaderboardEntry.cs
+++ Assets/Scripts/Leaderboard/LeaderboardEntry.cs
@@ -6,22 +6,22 @@ namespace Roguelike.Leaderboard
     public class LeaderboardEntry
     {
         private const string Anonymous = nameof(Anonymous);
-
-        public string PlayerName { get; private set; }
-        public string Score { get; private set; }
-        public string Rank { get; private set; }
-
+        
         public LeaderboardEntry(LeaderboardEntryResponse entry)
         {
             if (entry == null)
                 throw new ArgumentNullException(nameof(entry));
             
-            //todo : get avatar
-
             SetName(entry.player.publicName);
+            AvatarUrl = entry.player.profilePicture;
             Score = entry.score.ToString();
             Rank = $"{entry.rank}.";
         }
+
+        public string PlayerName { get; private set; }
+        public string AvatarUrl { get; private set; }
+        public string Score { get; private set; }
+        public string Rank { get; private set; }
 
         private void SetName(string name)
         {
--- Assets/Scripts/Leaderboard/LeaderboardEntryView.cs
+++ Assets/Scripts/Leaderboard/LeaderboardEntryView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Agava.YandexGames;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,19 +9,39 @@ namespace Roguelike.Leaderboard
     public class LeaderboardEntryView : MonoBehaviour
     {
         [Header("Player Info")]
-        [SerializeField] private Image _avatar;
+        [SerializeField] private RawImage _avatar;
         [SerializeField] private TextMeshProUGUI _name;
         [SerializeField] private TextMeshProUGUI _rank;
         [SerializeField] private TextMeshProUGUI _score;
-        
+
+        private bool _avatarDownloaded;
+
         public void SetData(LeaderboardEntry entry)
         {
             if (entry == null)
                 throw new ArgumentNullException(nameof(entry));
 
+            DownloadAvatar(entry.AvatarUrl);
+            _avatar.enabled = false;
             _rank.text = entry.Rank;
             _name.text = entry.PlayerName;
             _score.text = entry.Score;
+        }
+
+        private void DownloadAvatar(string avatarUrl)
+        {
+            if (_avatarDownloaded == false)
+            {
+                RemoteImage remoteImage = new(avatarUrl);
+                remoteImage.Download(SetAvatar);
+            }
+        }
+
+        private void SetAvatar(Texture2D texture)
+        {
+            _avatar.enabled = true;
+            _avatar.texture = texture;
+            _avatarDownloaded = true;
         }
     }
 }
