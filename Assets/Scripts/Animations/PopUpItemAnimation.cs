ba858684e28bacf0a4435cb1f2cbc7e43c95e0f1
Roman Ridel <ridel_rv@mail.ru>
2023-07-04 13:30:13 +0300

    Fix bug when weapon could pass through walls when thrown out

--- Assets/Scripts/Animations/PopUpItemAnimation.cs
+++ Assets/Scripts/Animations/PopUpItemAnimation.cs
@@ -13,27 +13,32 @@ namespace Roguelike.Animations
         [SerializeField] private float _jumpDistance;
 
         private Sequence _sequence;
-        
+        private Tween _landingTween;
+
         private void Start()
         {
             Vector3 defaultScale = transform.localScale;
             Vector3 jumpDistance = transform.forward * _jumpDistance;
-            
+
             PlayAnimation(defaultScale, jumpDistance);
         }
 
-        private void OnDisable() => _sequence.Kill();
-
-        private Vector3 CalculateGroundPosition(Vector3 currentPosition)
+        private void OnDisable()
         {
-            if (Physics.Raycast(currentPosition, Vector3.down, out RaycastHit hit))
-            {
-                Vector3 modelPosition = new(
-                    currentPosition.x,
-                    hit.point.y,
-                    currentPosition.z);
+            _sequence.Kill();
+            _landingTween.Kill();
+        }
 
-                return modelPosition;
+        private Vector3 CalculateGroundPosition(Vector3 position)
+        {
+            if (Physics.Raycast(position, Vector3.down, out RaycastHit hit))
+            {
+                Vector3 itemPosition = new(
+                    position.x,
+                    hit.point.y,
+                    position.z);
+
+                return itemPosition;
             }
 
             throw new ArgumentOutOfRangeException(nameof(transform.position), "Ground is out of reach");
@@ -42,15 +47,24 @@ namespace Roguelike.Animations
         private void PlayAnimation(Vector3 defaultScale, Vector3 jumpDistance)
         {
             _sequence = DOTween.Sequence();
-            
+
             _sequence.SetUpdate(false);
             _sequence.Append(transform
                 .DOScale(defaultScale, _scalingDuration)
                 .From(Vector3.zero));
 
-            _sequence.Insert(0, transform
-                .DOJump(transform.position + jumpDistance, _jumpPower, 1, _jumpDuration)
-                .SetEase(Ease.InQuad));
+            if (CheckGroundOn(transform.position + jumpDistance))
+            {
+                _sequence.Insert(0, transform
+                    .DOJump(transform.position + jumpDistance, _jumpPower, 1, _jumpDuration)
+                    .SetEase(Ease.InQuad));
+            }
+            else
+            {
+                _sequence.Insert(0, transform
+                    .DOJump(transform.position, _jumpPower, 1, _jumpDuration)
+                    .SetEase(Ease.InQuad));
+            }
 
             _sequence.onComplete = LandObjectToGround;
         }
@@ -58,10 +72,13 @@ namespace Roguelike.Animations
         private void LandObjectToGround()
         {
             Vector3 position = CalculateGroundPosition(transform.position);
-            
-            transform.DOMove(position, _landingDuration)
-                .SetEase(Ease.OutQuad)
-                .onComplete = () => enabled = false;
+
+            _landingTween = transform.DOMove(position, _landingDuration)
+                .SetEase(Ease.OutQuad);
+
+            _landingTween.onComplete = () => enabled = false;
         }
+
+        private bool CheckGroundOn(Vector3 position) => Physics.Raycast(position, Vector3.down);
     }
 }
